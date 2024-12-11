a915c7dc795ad4bb6755ebc096cd6daad8d3855f
Nouvy <95512507+Nouvy@users.noreply.github.com>
2024-12-11 14:53:59 +0100

    Push 2

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,7 +5,7 @@ import scala.io.StdIn
 def main(): Unit =
 
   //Constante non modifiable
-  val constante = "Hello";
+  val constante, test = "Hello";
   //constante = "Test";
 
   //Valeurs modifiables
@@ -14,9 +14,7 @@ def main(): Unit =
   var chaine = "Bonjour";
   var chaine2 = "Bonjour";
   chaine = "Test";
-
-
-
+  var resultat = 0;
   var choix = 1;
 
   while (choix != 0) {
@@ -24,7 +22,8 @@ def main(): Unit =
     println("1 - Test Scala");
     println("2 - Nombre entre 1 et 3")
     println("3 - Nombre entre 50 et 60")
-    println("4 - Table de multiplication jusqu'à 40")
+    println("4 - Table de multiplication jusqu'à 40 WHILE")
+    println("5 - Table de multiplication jusqu'à 40 FOR")
     println("0 - QUITTER")
     choix = StdIn.readLine().toInt;
 
@@ -63,6 +62,27 @@ def main(): Unit =
           else
             println("Bravo")
         }
+      case 4 =>
+        nombre = 0;
+        println("Merci de saisir un nombre :");
+        nombre = StdIn.readLine().toInt;
+        var compteur = 1;
+
+        while (compteur <= 40) {
+          resultat = (compteur * nombre);
+          println(s"$compteur x $nombre = $resultat");
+          compteur = compteur + 1;
+        }
+      case 5 =>
+        nombre = 0;
+        println("Merci de saisir un nombre :");
+        nombre = StdIn.readLine().toInt;
+        var max = 40;
+        for (i <- 1 to 40) {
+          resultat = (i * nombre);
+          println(s"$i x $nombre = $resultat");
+        }
+
   }
 
 
